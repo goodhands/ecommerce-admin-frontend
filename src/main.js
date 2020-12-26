7fef33c70792eb824dc61dd1ae4bb1747b5ef2c5
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-12-26 21:27:17 +0100

    add tailwind

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ Vue.config.productionTip = false
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common['Accept'] = 'application/json';
-// axios.defaults.headers.common['Authorization'] = "Bearer 6|gycoyXUCv3Fzmo3RA5VgQYHd5ujg1h9cxfeAFYBW";
 
 new Vue({
   render: h => h(App),
